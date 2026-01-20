66b02ba220eaa567f6e38cb9b25b61740621c024
Kaiz Dawson <kaizdawson@gmail.com>
2026-01-20 16:35:39 +0700

    download cv

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -84,35 +84,35 @@ const AboutSection = () => {
   };
 
   return (
-    <section className="text-white" id="about">
+    <section id="about" className="text-white scroll-mt-24">
       <div className="md:grid md:grid-cols-2 gap-8 items-start py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image src="/images/sep.jpg" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <div className="text-base lg:text-lg text-gray-300 leading-relaxed max-w-xl space-y-4">
-  <p>
-    I am a backend developer specializing in ASP.NET Core Web API.
-    I focus on building secure, scalable, and maintainable backend systems
-    with clean architecture and well-structured code.
-  </p>
+      <p>
+        I am a backend developer specializing in ASP.NET Core Web API.
+        I focus on building secure, scalable, and maintainable backend systems
+        with clean architecture and well-structured code.
+      </p>
 
-  <p>
-  I have experience working with relational databases such as SQL Server,
-  implementing authentication and authorization using JWT, and designing RESTful APIs
-  for real-world applications.
-</p>
+      <p>
+      I have experience working with relational databases such as SQL Server,
+      implementing authentication and authorization using JWT, and designing RESTful APIs
+      for real-world applications.
+      </p>
 
 
-  <p>
-    I am familiar with deploying backend services on Ubuntu/Linux VPS,
-    configuring environments, and setting up basic CI/CD pipelines
-    using GitHub Actions.
-  </p>
+      <p>
+        I am familiar with deploying backend services on Ubuntu/Linux VPS,
+        configuring environments, and setting up basic CI/CD pipelines
+        using GitHub Actions.
+      </p>
 
-  <p>
-    In addition, I also explore game development using Unity (2D) and C#,
-    focusing on gameplay programming and basic game mechanics.
-  </p>
+      <p>
+        In addition, I also explore game development using Unity (2D) and C#,
+        focusing on gameplay programming and basic game mechanics.
+      </p>
 </div>
 
 
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -54,7 +54,8 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/"
+              href="/cv/VoDangKhoa_CV.pdf"
+              download="VoDangKhoa_CV.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
